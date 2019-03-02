2e3fce78c3981440a219f588f9211293ebadf911
markwh1te <iamwh1temark@outlook.com>
2019-03-02 15:08:08 +0800

    add updated

--- Python/64.py
+++ Python/64.py
@@ -11,8 +11,9 @@ class Solution:
             distance = self.minPathSumHelper(grid, x - 1, y)
             r = grid[x][y] + distance
         else:
-            r = grid[x][y] + min(self.minPathSumHelper(grid, x, y - 1),
-                                 self.minPathSumHelper(grid, x - 1, y))
+            r = grid[x][y] + min(
+                self.minPathSumHelper(grid, x, y - 1),
+                self.minPathSumHelper(grid, x - 1, y))
         self.cache[(x, y)] = r
         return r
 
@@ -30,12 +31,5 @@ class Solution:
 if __name__ == '__main__':
     from util import Test
     t = Test(Solution().minPathSum)
-    input = [
-        [1, 3, 1],
-        [1, 5, 1],
-        [4, 2, 1]
-    ]
-    input2 = [[7, 0, 8, 8, 0, 3, 5, 8, 5, 4], [4, 1, 2, 9,
-                                               9, 6, 0, 8, 6, 9], [9, 7, 1, 1, 0, 1, 2, 4, 1, 7]]
-    t.equal(7, input)
-    t.equal(13, input2)
+    test = [[1, 3, 1], [1, 5, 1], [4, 2, 1]]
+    t.equal(7, test)
