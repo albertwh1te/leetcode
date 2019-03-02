cc413d07ea171ce5a90b07da6c872a6ab56b9b3e
markwh1te <iamwh1temark@outlook.com>
2019-03-02 22:59:51 +0800

    add another solution for shortest path

--- Python/64.py
+++ Python/64.py
@@ -28,20 +28,25 @@ class Solution:
         return self.minPathSumHelper(grid, x, y)
 
     def minPathSum2(self, grid):
-        dp = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        column = len(grid)
+        row = len(grid[0])
+        dp = [[0 for _ in range(row)] for _ in range(column)]
         dp[-1][-1] = grid[-1][-1]
-        for i in range(len(grid[0]) - 2, 0, -1):
-            dp[i][-1] = dp[i - 1][-1] + grid[i][-1]
-        for j in range(len(grid) - 2, 0, -1):
-            dp[-1][j] = dp[-1][j - 1] + grid[-1][j]
-        from util import print_matrix
-        print_matrix(dp)
-        print_matrix(grid)
+        for i in range(column-2, -1, -1):
+            dp[i][-1] = dp[i + 1][-1] + grid[i][-1]
+        for j in range(row-2, -1, -1):
+            dp[-1][j] = dp[-1][j + 1] + grid[-1][j]
+
+        for i in range(column-2, -1, -1):
+            for j in range(row-2, -1, -1):
+                dp[i][j] = min(dp[i][j+1], dp[i+1][j]) + grid[i][j]
+
         return dp[0][0]
 
 
 if __name__ == '__main__':
     from util import Test
+    t = Test(Solution().minPathSum)
     t = Test(Solution().minPathSum2)
     test = [[1, 3, 1], [1, 5, 1], [4, 2, 1]]
     t.equal(7, test)
