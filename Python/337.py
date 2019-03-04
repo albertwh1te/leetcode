7f69b7d602c976110467d56da33ba991d3212281
markwh1te <iamwh1temark@outlook.com>
2019-03-04 19:22:51 +0800

    add lt solution

---
+++ Python/337.py
@@ -0,0 +1,62 @@
+# 337. House Robber III
+# The thief has found himself a new place for his thievery again.
+#  There is only one entrance to this area,
+#  called the "root." Besides the root,
+#  each house has one and only one parent house.
+#  After a tour,
+#  the smart thief realized that "all houses in this place forms a binary tree".
+#  It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+# Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+# Example 1:
+
+# Input: [3,2,3,null,3,null,1]
+
+#      3
+#     / \
+#    2   3
+#     \   \
+#      3   1
+
+# Output: 7
+# Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+# Example 2:
+
+# Input: [3,4,5,1,3,null,1]
+
+#      3
+#     / \
+#    4   5
+#   / \   \
+#  1   3   1
+
+# Output: 9
+# Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def rob_help(self, node, flag) -> int:
+        if not node:
+            return 0
+        subtree = self.rob_help(node.left, False) + self.rob_help(
+            node.right, False)
+
+        if flag:
+            return subtree
+
+        else:
+            return max(
+                subtree,
+                node.val + self.rob_help(node.left, True) + self.rob_help(
+                    node.right, True))
+
+    def rob(self, root: TreeNode) -> int:
+        return self.rob_help(root, False)
