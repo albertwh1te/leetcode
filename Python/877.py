386c7be7a2009bad2841bc4859897fb587e7c9d3
markwh1te <iamwh1temark@outlook.com>
2019-03-07 14:28:32 +0800

    add 877

--- Python/877.py
+++ Python/877.py
@@ -1,11 +1,21 @@
 # 877. Stone Game
-# lex and Lee play a game with piles of stones.  There are an even number of piles arranged in a row, and each pile has a positive integer number of stones piles[i].
+# lex and Lee play a game with piles of stones.
+#   There are an even number of piles arranged in a row,
+#  and each pile has a positive integer number of stones piles[i].
 
-# The objective of the game is to end with the most stones.  The total number of stones is odd, so there are no ties.
+# The objective of the game is to end with the most stones.
+#   The total number of stones is odd, so there are no ties.
 
-# Alex and Lee take turns, with Alex starting first.  Each turn, a player takes the entire pile of stones from either the beginning or the end of the row.  This continues until there are no more piles left, at which point the person with the most stones wins.
+# Alex and Lee take turns,
+#  with Alex starting first.
+#  Each turn, a player takes the entire pile of stones from either the beginning or the end of the row.
 
-# Assuming Alex and Lee play optimally, return True if and only if Alex wins the game.
+#  This continues until there are no more piles left,
+
+#  at which point the person with the most stones wins.
+
+# Assuming Alex and Lee play optimally,
+#  return True if and only if Alex wins the game.
 
 # Example 1:
 
@@ -24,3 +34,10 @@
 # piles.length is even.
 # 1 <= piles[i] <= 500
 # sum(piles) is odd.
+from typing import List
+
+
+class Solution:
+    # Mathematics will always prevail!
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
