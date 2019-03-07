3013181e73de78b21dc952929869a9f1a70bd24a
markwh1te <iamwh1temark@outlook.com>
2019-03-07 14:30:10 +0800

    update 877

--- Python/877.py
+++ Python/877.py
@@ -39,5 +39,12 @@ from typing import List
 
 class Solution:
     # Mathematics will always prevail!
+    # Alex clearly always wins the 2 pile game. With some effort, we can see that she always wins the 4 pile game.
+
+    # If Alex takes the first pile initially, she can always take the third pile. If she takes the fourth pile initially, she can always take the second pile. At least one of first + third, second + fourth is larger, so she can always win.
+
+    # We can extend this idea to N piles. Say the first, third, fifth, seventh, etc. piles are white, and the second, fourth, sixth, eighth, etc. piles are black. Alex can always take either all white piles or all black piles, and one of the colors must have a sum number of stones larger than the other color.
+
+    # Hence, Alex always wins the game.
     def stoneGame(self, piles: List[int]) -> bool:
         return True
