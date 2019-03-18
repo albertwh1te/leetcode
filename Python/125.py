04d93b29ef9bc8e413b730e7523e1afee5422985
markwh1te <iamwh1temark@outlook.com>
2019-03-18 11:48:40 +0800

    add 125

--- Python/125.py
+++ Python/125.py
@@ -19,3 +19,27 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalpha(
+            ) and not s[left].isdigit():
+                left += 1
+            while left < right and not s[right].isalpha(
+            ) and not s[right].isdigit():
+                right -= 1
+
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+
+if __name__ == '__main__':
+    from util import Test
+    t = Test(Solution().isPalindrome)
+    t.equal(True, "A man, a plan, a canal: Panama")
+    t.equal(False, "race a car")
+    t.equal(False, "0P")
+    t.equal(False, "ab2a")
