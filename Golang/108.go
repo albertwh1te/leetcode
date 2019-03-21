89edf40c6272d99b69bbcd73457daa422d7c1171
markwh1te <iamwh1temark@outlook.com>
2019-03-21 15:58:07 +0800

    add 108

---
+++ Golang/108.go
@@ -0,0 +1,22 @@
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	if len(nums) == 1 {
+		return &TreeNode{nums[0], nil, nil}
+	}
+	index := len(nums) / 2
+	root := TreeNode{nums[index], nil, nil}
+	root.Left = sortedArrayToBST(nums[0:index])
+	root.Right = sortedArrayToBST(nums[index+1:])
+	return &root
+}
