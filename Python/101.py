7d4cf4959c75e8b2847f03f2a48809982a9476ce
markwh1te <iamwh1temark@outlook.com>
2019-03-21 15:15:58 +0800

    add 101

---
+++ Python/101.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root == None:
+            return True
+        return self.isMirror(root.left, root.right)
+
+    def isMirror(self, left, right):
+        if not left or not right:
+            return left == right
+        if left.val != right.val:
+            return False
+        return self.isMirror(left.right, right.left) and self.isMirror(
+            left.left, right.right)
