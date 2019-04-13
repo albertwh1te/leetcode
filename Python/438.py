fac64ce575f93f4b80905b68d74601f95f3302a8
markwh1te <iamwh1temark@outlook.com>
2019-04-13 17:41:33 +0800

    add solution 438

--- Python/438.py
+++ Python/438.py
@@ -30,6 +30,8 @@
 # The substring with start index = 0 is "ab", which is an anagram of "ab".
 # The substring with start index = 1 is "ba", which is an anagram of "ab".
 # The substring with start index = 2 is "ab", which is an anagram of "ab".
+
+# Solution : slide window
 from typing import List
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
