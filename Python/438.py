9fe14ab5c40c24083690e431cbcb6c61c771a477
markwh1te <iamwh1temark@outlook.com>
2019-04-14 18:37:29 +0800

    finished 438

--- Python/438.py
+++ Python/438.py
@@ -32,6 +32,35 @@
 # The substring with start index = 2 is "ab", which is an anagram of "ab".
 
 # Solution : slide window
+# https: // blog.csdn.net/yy254117440/article/details/53025142
 from typing import List
+
+
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        # corner case
+        if len(s) == 0 or len(p) == 0:
+            return []
+        result = []
+        length = len(p)
+        char_arr1 = [0 for _ in range(255)]
+        char_arr2 = [0 for _ in range(255)]
+        for i in p:
+            char_arr1[ord(i)] += 1
+        for i, v in enumerate(s):
+            if i >= length:
+                char_arr2[ord(s[i-length])] -= 1
+            char_arr2[ord(v)] += 1
+            if char_arr1 == char_arr2:
+                result.append(i+1-length)
+        return result
+
+
+if __name__ == '__main__':
+    from util import Test
+    s = Solution()
+    t = Test(s.findAnagrams)
+    result = [0, 6]
+    t.equal(result, "cbaebabacd", "abc")
+    result = [0, 1, 2]
+    t.equal(result, "abab", "ab")
