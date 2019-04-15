c555ba110f687a8f7fd5632e253591ee39ef7cde
markwh1te <iamwh1temark@outlook.com>
2019-04-15 20:36:18 +0800

    fix travis

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "iamwh1temark@outlook.com"
+  git config --global user.name "MarkWh1te"
 }
 
 commit_website_files() {
   git add README.md
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER update README.md"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MarkWh1te/leetcode/ > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/MarkWh1te/leetcode/ > /dev/null 2>&1
   git push --quiet origin master 
 }
 
