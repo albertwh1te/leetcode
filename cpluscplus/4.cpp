833f31951d18f3292845413f204a2c59a60cadeb
markwh1te <iamwh1temark@outlook.com>
2019-04-25 22:10:56 +0800

    :coffee: half done 4

--- cpluscplus/4.cpp
+++ cpluscplus/4.cpp
@@ -37,6 +37,19 @@ int findKth(vector<int> &a, int a_start, int a_end, vector<int> &b, int b_start,
     }
     int pa = min(k / 2, a_end - a_start + 1);
     int pb = k - pa;
+    if (a[a_start + pa - 1] == b[b_start + pb - 1])
+    {
+        return a[a_start + pa - 1];
+    }
+    if (a[a_start + pa - 1] < b[b_start + pb - 1])
+    {
+        return findKth(a, a_start + pa, a_end, b, b_start, b_end, k - pa);
+    }
+
+    if (a[a_start + pa - 1] > b[b_start + pb - 1])
+    {
+        return findKth(a, a_start, a_end, b, b_start + pa, b_end, k - pa);
+    }
 }
 
 class Solution
@@ -48,14 +61,14 @@ class Solution
         int n = nums2.size();
         int total = m + n;
         double a = (double)findKth(nums1, 0, m - 1, nums2, 0, n - 1, total / 2 + 1);
-        if (total % 2 == 0)
+        if (total % 2)
         {
-            cout << "odd_a:" << a << "total" << total << endl;
+            cout << "odd:" << a << "total" << total << endl;
             return a;
         }
 
         int b = findKth(nums1, 0, m - 1, nums2, 0, n - 1, total / 2);
-        cout << "even_b:" << b << endl;
+        cout << "even :" << b << endl;
         return (a + b) * 0.5;
     }
 };
@@ -67,9 +80,9 @@ int main()
     Solution s = Solution();
     double answer = s.findMedianSortedArrays(test1, test2);
     cout << "should be 2.0: and get " << answer << endl;
-    // test1 = {1, 2};
-    // test2 = {3, 4};
-    // double answer2 = s.findMedianSortedArrays(test1, test2);
-    // cout << "should be 2.5:" << answer2 << endl;
+    test1 = {1, 2};
+    test2 = {3, 4};
+    double answer2 = s.findMedianSortedArrays(test1, test2);
+    cout << "should be 2.5:" << answer2 << endl;
     return 0;
 }
