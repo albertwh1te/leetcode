53c9931f3f91035a708d6f6582bc6e597430e61f
markwh1te <iamwh1temark@outlook.com>
2019-04-26 17:44:16 +0800

    :pen:  add C++ Solution for 4 🐝

--- cpluscplus/4.cpp
+++ cpluscplus/4.cpp
@@ -3,26 +3,9 @@
 #include <vector>
 using namespace std;
 
-// A = {1，3，5，7}；B = {2，4，6，8，9，10}；
-// 如果要求第7个小的数，
-// A数列的元素个数为4，
-// B数列的元素个数为6；
-// k/2 = 7/2 = 3，
-// 而A中的第3个数A[2]=5；
-// B中的第3个数B[2]=6；
-// 而A[2]<B[2]；
-// 则A[0]，A[1]，A[2]中必然不可能有第7个小的数。
-// 因为A[2]<B[2]，
-// 所以比A[2]小的数最多可能为A[0],
-//  A[1], B[0], B[1]
-// 这四个数，
-// 也就是说A[2]最多可能是第5个大的数，
-// 由于我们要求的是getKth(A, B, 7)；
-// 现在就变成了求getKth(A', B, 4)；
-// 即A' = {7}；
-// B不变，求这两个数列的第4个小的数，因为A[0]，A[1]，A[2]中没有解，所以我们直接删掉它们就可以了。这个可以使用递归来实现。
 int findKth(vector<int> &a, int a_start, int a_end, vector<int> &b, int b_start, int b_end, int k)
 {
+    // corner case
     if (a_end - a_start > b_end - b_start)
     {
         return findKth(b, b_start, b_end, a, a_start, a_end, k);
@@ -33,8 +16,9 @@ int findKth(vector<int> &a, int a_start, int a_end, vector<int> &b, int b_start,
     }
     if (k == 1)
     {
-        return min(a[0], b[0]);
+        return min(a[a_start], b[b_start]);
     }
+
     int pa = min(k / 2, a_end - a_start + 1);
     int pb = k - pa;
     if (a[a_start + pa - 1] == b[b_start + pb - 1])
@@ -45,11 +29,7 @@ int findKth(vector<int> &a, int a_start, int a_end, vector<int> &b, int b_start,
     {
         return findKth(a, a_start + pa, a_end, b, b_start, b_end, k - pa);
     }
-
-    if (a[a_start + pa - 1] > b[b_start + pb - 1])
-    {
-        return findKth(a, a_start, a_end, b, b_start + pa, b_end, k - pa);
-    }
+    return findKth(a, a_start, a_end, b, b_start + pa, b_end, k - pa);
 }
 
 class Solution
@@ -63,12 +43,9 @@ class Solution
         double a = (double)findKth(nums1, 0, m - 1, nums2, 0, n - 1, total / 2 + 1);
         if (total % 2)
         {
-            cout << "odd:" << a << "total" << total << endl;
             return a;
         }
-
         int b = findKth(nums1, 0, m - 1, nums2, 0, n - 1, total / 2);
-        cout << "even :" << b << endl;
         return (a + b) * 0.5;
     }
 };
