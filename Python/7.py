f7c3258a6069d9571c67860084d9997c75b685f0
markwh1te <iamwh1temark@outlook.com>
2019-05-09 19:30:38 +0800

    🖊 update Python Solution for 7

--- Python/7.py
+++ Python/7.py
@@ -22,34 +22,20 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
-        flag = False
-        if x < 0:
-            x = abs(x)
-            flag = True
-
-        digits = []
-        while x > 0:
-            digits.append(x % 10)
-            x = x // 10
-
         result = 0
-        for i, v in enumerate(digits):
-            if v == 0:
-                continue
-            else:
-                result += v * 10**((len(digits) - i) - 1)
-                if result > 2**31 - 1:
-                    return 0
-        if flag:
-            return result * (-1)
-        else:
-            return result
+        while x != 0:
+            print(x, "))", result)
+            result = result * 10 + x % 10
+            if abs(result) > (2**31 - 1):
+                return 0
+            x = x // 10
+        return result
 
 
 if __name__ == '__main__':
     from util import Test
     t = Test(Solution().reverse)
-    t.equal(321, 123)
+    # t.equal(321, 123)
     t.equal(-321, -123)
-    t.equal(0, 1534236469)
-    t.equal(0, 1563847412)
+    # t.equal(0, 1534236469)
+    # t.equal(0, 1563847412)
