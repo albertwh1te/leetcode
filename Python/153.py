cce25a261fb58a773522693a33f4d3ce3cd1479b
markwh1te <iamwh1temark@outlook.com>
2019-08-24 16:46:36 +0800

    add Python Solution for 153

--- Python/153.py
+++ Python/153.py
@@ -16,18 +16,32 @@
 
 # Input: [4,5,6,7,0,1,2]
 # Output: 0
+
 from util import equal
 from typing import List
+
+## Original Array : 1,2,3,4,5,6,7
+
+## CASE 1:  6 7 1 2(mid) 3 4 5
+
+## CASE 1 mid < end ,nums[mid:end] is sorted  minium is in nums[start,mid]
+
+## CASE 2:  3 4 5 6(mid) 7 1 2
+
+## CASE 2 mid >= end, nums[start: mid] is sorted minum is in nums[mid+1,end]
 
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         left = 0
         right = len(nums) - 1
-        while 1:
-            mid = left + (right - left) >> 1
-            if nums[mid] < nums[mid - 1]:
-                return nums[mid]
+        while left <= right:
+            mid = left + ((right - left) >> 1)
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+        return nums[mid]
 
 
 def main():
