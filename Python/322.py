1c264615c21d5faac3d5da98a6bb6d7b34669afe
MarkWh1te <iamwh1temark@outlook.com>
2020-02-28 10:48:58 +0800

    delete use less variable

--- Python/322.py
+++ Python/322.py
@@ -6,7 +6,6 @@ class Solution:
         dp = [-1 for _ in range(amount + 1)]
         dp[0] = 0
         for i in range(amount + 1):
-            tmp = []
             for coin in coins:
                 if (i - coin) >= 0 and dp[i - coin] >= 0:
                     if dp[i] == -1:
