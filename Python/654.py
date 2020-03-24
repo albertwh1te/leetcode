e494464c3a7f7a20141833f66c0d8c1702142cdf
MarkWh1te <iamwh1temark@outlook.com>
2020-03-24 22:34:20 +0800

    Update Python 654

--- Python/654.py
+++ Python/654.py
@@ -6,9 +6,14 @@ class Solution(object):
         """
         if not nums:
             return None
-        maximum = max(nums)
-        left = nums[0:nums.index(maximum)]
-        right = nums[nums.index(maximum) + 1:]
+        maximum = float("-inf")
+        max_index = 0
+        for index, value in enumerate(nums):
+            if value > maximum:
+                maximum = value
+                max_index = index
+        left = nums[0:max_index]
+        right = nums[max_index + 1 :]
         root = TreeNode(maximum)
         root.left = self.constructMaximumBinaryTree(left)
         root.right = self.constructMaximumBinaryTree(right)
