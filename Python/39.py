1776598f045a5c1a807d1762dd5dd883cf3cfad1
MarkWh1te <iamwh1temark@outlook.com>
2020-05-09 14:03:04 +0800

    Add Python  Solution for 39 and 448

--- Python/39.py
+++ Python/39.py
@@ -1,3 +1,34 @@
+# 39. Combination Sum
+
+# Given a set of candidate numbers (candidates) (without duplicates) and
+#  a target number (target), find all unique combinations in candidates
+#  where the candidate numbers sums to target.
+
+# The same repeated number may be chosen from candidates unlimited number of times.
+
+# Note:
+
+# All numbers (including target) will be positive integers.
+# The solution set must not contain duplicate combinations.
+# Example 1:
+
+# Input: candidates = [2,3,6,7], target = 7,
+# A solution set is:
+# [
+#   [7],
+#   [2,2,3]
+# ]
+# Example 2:
+
+# Input: candidates = [2,3,5], target = 8,
+# A solution set is:
+# [
+#   [2,2,2,2],
+#   [2,3,3],
+#   [3,5]
+# ]
+
+
 class Solution(object):
     def combinationSum(self, candidates, target):
         """
@@ -19,18 +50,12 @@ class Solution(object):
         return results
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from util import Test
+
     s = Solution()
     t = Test(s.combinationSum)
-    r1 = [
-        [2, 2, 3],
-        [7]
-    ]
+    r1 = [[2, 2, 3], [7]]
     t.equal(r1, [2, 3, 6, 7], 7)
-    r2 = [
-        [2, 2, 2, 2],
-        [2, 3, 3],
-        [3, 5]
-    ]
+    r2 = [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
     t.equal(r2, [2, 3, 5], 8)
--- Python/448.py
+++ Python/448.py
@@ -13,15 +13,40 @@
 
 # Output:
 # [5,6]
+from typing import List
+
+
+from collections import Counter
 
 
 class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    def findDisappearedNumbers_1(self, nums: List[int]) -> List[int]:
+        c = Counter(nums)
         result = []
+        for i in range(1, len(nums) + 1):
+            if c.get(i) == None:
+                result.append(i)
+        return result
+
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            idx = abs(nums[i]) - 1
+            if nums[idx] > 0:
+                nums[idx] = -nums[idx]
+
+        result = []
+        for i, v in enumerate(nums):
+            if v > 0:
+                result.append(i + 1)
+
+        return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from util import Test
+
     s = Solution()
     t = Test(s.findDisappearedNumbers)
+    t.equal([5, 6], [4, 3, 2, 7, 8, 2, 3, 1])
+    t = Test(s.findDisappearedNumbers_1)
     t.equal([5, 6], [4, 3, 2, 7, 8, 2, 3, 1])
